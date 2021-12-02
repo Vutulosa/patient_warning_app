1e900ead89688d5b73bcdcdedf366de0ef100b6c
Vutulosa <59256117+Vutulosa@users.noreply.github.com>
2021-12-02 16:19:48 +0000

    Search Screen

--- lib/Screens/SearchScreen/Components/body.dart
+++ lib/Screens/SearchScreen/Components/body.dart
@@ -30,47 +30,79 @@ class _SearchBodyState extends State<SearchBody> {
 
         final data = snapshot.requireData;
 
-        return Column(children: [
-          Padding(
-            padding: const EdgeInsetsDirectional.fromSTEB(15, 15, 15, 15),
-            child: TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Search',
-                border: OutlineInputBorder(),
-                suffixIcon: Icon(
-                  Icons.search,
-                ),
-              ),
-            ),
-          ),
-          ListView.separated(
-            shrinkWrap: true,
-            itemCount: data.size,
-            separatorBuilder: (context, index) {
-              return const Divider();
-            },
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: const Icon(
-                  Icons.movie,
-                  color: Colors.teal,
-                ),
-                title: Text(data.docs[index]['name']),
-                trailing: const Icon(Icons.arrow_forward),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          MediaDetailScreen(media: data.docs[index]),
-                    ),
-                  );
-                },
-              );
-            },
-          )
-        ]);
+        return MediaList(data: data);
       },
     );
+  }
+}
+
+class MediaList extends StatefulWidget {
+  const MediaList({Key? key, required this.data}) : super(key: key);
+  final QuerySnapshot<Object?> data;
+
+  @override
+  _MediaListState createState() => _MediaListState();
+}
+
+class _MediaListState extends State<MediaList> {
+  String name = '';
+
+  @override
+  Widget build(BuildContext context) {
+    List<QueryDocumentSnapshot<Object?>> list = [];
+
+    for (int i = 0; i < widget.data.size; i++) {
+      if (name.isEmpty) {
+        list = widget.data.docs;
+        break;
+      }
+      if (widget.data.docs[i]['name'].toString().toUpperCase().contains(name)) {
+        list.add(widget.data.docs[i]);
+      }
+    }
+
+    return Column(children: [
+      Padding(
+        padding: const EdgeInsetsDirectional.fromSTEB(15, 15, 15, 15),
+        child: TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Search',
+              border: OutlineInputBorder(),
+              suffixIcon: Icon(
+                Icons.search,
+              ),
+            ),
+            onChanged: (String value) {
+              setState(() {
+                name = value.toUpperCase();
+              });
+            }),
+      ),
+      ListView.separated(
+        shrinkWrap: true,
+        itemCount: list.length,
+        separatorBuilder: (context, index) {
+          return const Divider();
+        },
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: const Icon(
+              Icons.movie,
+              color: Colors.teal,
+            ),
+            title: Text(list[index]['name']),
+            trailing: const Icon(Icons.arrow_forward),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MediaDetailScreen(media: list[index]),
+                ),
+              );
+            },
+          );
+        },
+      )
+    ]);
   }
 }
