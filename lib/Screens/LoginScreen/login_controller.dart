0340db9a2c974116b5f9db3d5ab178024ad5c10d
Vutulosa <59256117+Vutulosa@users.noreply.github.com>
2021-12-04 16:50:35 +0000

    Login

--- lib/Screens/LoginScreen/login_controller.dart
+++ lib/Screens/LoginScreen/login_controller.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:patient_warning_app/Screens/HomeScreen/home_screen.dart';
 import 'package:patient_warning_app/Screens/LoginScreen/Screens/login_screen.dart';
 
 class LoginController extends StatefulWidget {
@@ -11,6 +15,22 @@ class LoginController extends StatefulWidget {
 class _LoginControllerState extends State<LoginController> {
   @override
   Widget build(BuildContext context) {
-    return LoginScreen();
+    return StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Container();
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            return const Text('Done.');
+          } else if (snapshot.hasError) {
+            return const Text("Error has occured");
+          } else {
+            if (snapshot.data != null) {
+              return const HomeScreen();
+            } else {
+              return const LoginScreen();
+            }
+          }
+        });
   }
 }
